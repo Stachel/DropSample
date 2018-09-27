ed6d75452466c6070d114cf31eff154cf465e3d6
RinaStachel <hempa@inbox.ru>
2018-09-27 23:17:44 +0300

    add font support

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -4,18 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 class MainMenuScreen implements Screen {
 
     final DropGame game;
     OrthographicCamera camera;
+    public static final String ALL_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            + "abcdefghijklmnopqrstuvwxyz"
+            + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+            + "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+            + "1234567890.,:;_¡!¿?\"'+-*/()[]={}";
 
+    FreeTypeFontGenerator generator;
+    BitmapFont fontRusso;
 
     public MainMenuScreen(final DropGame game) {
         this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("RussoOne.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 16;
+        parameter.characters = ALL_CHARACTERS;
+        fontRusso = generator.generateFont(parameter);
+        generator.dispose();
     }
 
     @Override
@@ -32,8 +48,8 @@ class MainMenuScreen implements Screen {
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        fontRusso.draw(game.batch, "Welcome to Drop!", 100, 150);
+        fontRusso.draw(game.batch, "Старт!", 100, 100);
         game.batch.end();
 
         if (Gdx.input.isTouched()){
@@ -65,6 +81,5 @@ class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
     }
 }
